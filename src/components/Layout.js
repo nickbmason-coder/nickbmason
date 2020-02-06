404052dba53bd6eacf1a626301a184b010682b3d
Michael Maurer <mjmaurer777@gmail.com>
2020-02-06 01:15:38 -0500

    skip link for accessability

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,6 +9,23 @@ const BodyContainer = styled.div`
   ${tw`h-auto`}
 `;
 
+const MainContainer = styled.main`
+  ${tw`h-auto`}
+`;
+
+const SkipLink = styled.a`
+  position: absolute;
+  top: -40px;
+  left: 0;
+  background: #000000;
+  color: white;
+  padding: 8px;
+  z-index: 100;
+  &:focus {
+    top: 0;
+  }
+`;
+
 class Layout extends React.Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
@@ -20,12 +37,13 @@ class Layout extends React.Component {
     const { children, path } = this.props;
     return (
       <BodyContainer>
+        <SkipLink className="skip-link" href="#main" />
         <Helmet titleTemplate="%s | Nick Mason" defaultTitle="Nick Mason">
           <meta charSet="utf-8" />
           {/* <link rel="canonical" href="http://nickbmason.com" /> */}
         </Helmet>
         <NavBar path={path} />
-        {children}
+        <MainContainer id="main">{children}</MainContainer>
       </BodyContainer>
     );
   }
